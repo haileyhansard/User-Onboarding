ad015168e993afffcb06161b3729ac3236bc561f
haileyhansard <haileyhansard@gmail.com>
2020-06-18 15:25:43 -0700

    Added formSchema.js

--- user-onboarding/src/formSchema.js
+++ user-onboarding/src/formSchema.js
@@ -1,1 +1,22 @@
-import * as Yup from 'yup'
+import * as Yup from 'yup';
+
+const formSchema = Yup.object().shape({
+    name: Yup
+    .string()
+    .required("Name is required"),
+
+    email: Yup
+    .string()
+    .email("Must include a valid email address")
+    .required("Must include an email address"),
+
+    password: Yup
+    .string()
+    .required("Must include a password under 12 characters long"),
+
+    terms: Yup
+    .boolean()
+    .oneOf([true], "Please agree to the terms of use")
+})
+
+export default formSchema;
