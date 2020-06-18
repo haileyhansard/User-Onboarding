30ef15acde94b6c0809e436b2f4b23499b305e3f
haileyhansard <haileyhansard@gmail.com>
2020-06-18 14:22:58 -0700

    Form set up, User page, Form page established, returning info on front end

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import Form from './Form'
+import User from './User'
 
-function App() {
+const initialMembers = []
+const initialFormValues = {
+  name: '',
+  email: '',
+}
+
+
+export default function App() {
+  const [members, setMembers] = useState(initialMembers)
+  const [formValues, setFormValues] = useState(initialFormValues)
+  const [error, setError] = useState('')
+
+  const onInputChange = evt => {
+    const {name, value} = evt.target;
+    setFormValues(formValues => ({
+      ...formValues,
+      [name]:value,
+    }));
+  }
+
+  const onSubmit = evt => {
+    evt.preventDefault()
+    if (!formValues.name || !formValues.email){
+      setError("Oops, please fill out all information.");
+    } else{
+      const newMember = {
+        name: formValues.name.trim(),
+        email: formValues.email.trim()
+      }
+      setMembers(members => {
+        return [...members, newMember];
+      });
+      setError('');
+      setFormValues(initialFormValues);
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to the Team! 
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <div>
+        <Form 
+        values={formValues}
+        onInputChange={onInputChange}
+        onSubmit={onSubmit}
+        error={error}
+        />
+        {members.map(member => {
+          return(
+            <User key={member.name} details={member} />
+          )
+        })
+        }
+      </div>
     </div>
   );
 }
 
-export default App;
+
